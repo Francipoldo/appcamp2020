4b63c89c2fb74e6185d82e66a97e45f2ab5da5b1
Francipoldo <francescocinelli08@gmail.com>
2020-07-20 11:18:21 +0200

    7 files modified, 3 added, 3 deleted

--- app/app.module.ts
+++ app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
-import { AboutPage } from '../pages/about/about';
+import { InsertPage } from '../pages/insert/insert';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
@@ -11,7 +11,7 @@ import { TabsPage } from '../pages/tabs/tabs';
 @NgModule({
   declarations: [
     MyApp,
-    AboutPage,
+    InsertPage,
     ContactPage,
     HomePage,
     TabsPage
@@ -23,7 +23,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    AboutPage,
+    InsertPage,
     ContactPage,
     HomePage,
     TabsPage
